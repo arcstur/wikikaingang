2ea030eb2786ecd5367596472152c6d0c3e66bd8
arcstur <arcstur@gmail.com>
2026-03-09 15:21:42 -0300

    feat: lista automática de verbetes

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Wikikaingáng scripts
 
 ## Contagem de artigos
 
-Nix shell para dependências: `nix-shell -p python312Packages.requests python312Packages.pandas python312Packages.matplotlib python312Packages.seaborn`
+Nix shell para dependências: `nix-shell -p python312Packages.requests python312Packages.pandas python312Packages.matplotlib python312Packages.seaborn python312Packages.beautifulsoup4`
--- contagem_artigos.py
+++ contagem_artigos.py
@@ -1,5 +1,14 @@
+# dependencies = [
+#   "requests",
+#   "pandas",
+#   "matplotlib",
+#   "seaborn",
+#   "beautifulsoup4",
+# ]
+
 from datetime import datetime
 from datetime import date
+from bs4 import BeautifulSoup
 import csv
 
 import requests
@@ -9,143 +18,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 API = "https://incubator.wikimedia.org/w/rest.php/v1/"
-HEADERS = {
-    "User-Agent": "Arcstur Scripts (https://meta.wikimedia.org/wiki/User:Arcstur)",
-}
-
-# TODO: obter essa lista automaticamente
-# obtido de <https://incubator.wikimedia.org/wiki/Special:PrefixIndex?prefix=Wp%2Fkgp%2F&namespace=0&hideredirects=1>
-titles = [
-    "Wp/kgp/'Ó To Vẽme",
-    "Wp/kgp/500 Prỹg",
-    "Wp/kgp/Acento gráfico fóg vĩ ki, kar Kanhgág vĩ ki",
-    "Wp/kgp/Brasil",
-    "Wp/kgp/Brincadeira corrida do saco( Morsa kãkã jẽ kỹ, vẽnhvó)",
-    "Wp/kgp/Dentro e fora ( Kãki, pãtã sỹn)",
-    "Wp/kgp/Farĩnh totor",
-    "Wp/kgp/Fua tu vẽme",
-    "Wp/kgp/Fuva",
-    "Wp/kgp/Fuva to vẽme",
-    "Wp/kgp/FÁG-TO VÃME",
-    "Wp/kgp/Fág",
-    "Wp/kgp/Fág-Nhin",
-    "Wp/kgp/Fág tynyr",
-    "Wp/kgp/Fág tỹ kyfe",
-    "Wp/kgp/Fãfãn Vãme",
-    "Wp/kgp/Fójin Vãme",
-    "Wp/kgp/Fẽfẽn",
-    "Wp/kgp/Fỹj",
-    "Wp/kgp/Gangao",
-    "Wp/kgp/Gangao vẽme",
-    "Wp/kgp/Gangavo",
-    "Wp/kgp/Garĩnh nug",
-    "Wp/kgp/Garĩnh nĩsóg",
-    "Wp/kgp/Garĩnh ta fór",
-    "Wp/kgp/Go or",
-    "Wp/kgp/Goj tu vẽme",
-    "Wp/kgp/Goj tu vẽmẽ",
-    "Wp/kgp/Gren",
-    "Wp/kgp/Gren to vẽme",
-    "Wp/kgp/Grun",
-    "Wp/kgp/Grũ",
-    "Wp/kgp/Gufã",
-    "Wp/kgp/Gãr kuka tu vãme",
-    "Wp/kgp/Gãr ránrár",
-    "Wp/kgp/Gãr ránrár tu vãme",
-    "Wp/kgp/Gãr ránrár tỹ ẽmĩ",
-    "Wp/kgp/Gónvẽ Vãme",
-    "Wp/kgp/Gĩr kar kyrũ sĩ kar kyrũ",
-    "Wp/kgp/Hẽrig tu vãme",
-    "Wp/kgp/Inh jamré fag",
-    "Wp/kgp/Jóhó",
-    "Wp/kgp/KUSÃGKE RĨR",
-    "Wp/kgp/Ka tỹ gatu to pẽg",
-    "Wp/kgp/Kajér",
-    "Wp/kgp/Kajẽr tỹ mĩg mré vẽnh génh kãme",
-    "Wp/kgp/Kanhgang jyjy",
-    "Wp/kgp/Kanhgág Vẽnhkagta",
-    "Wp/kgp/Kanhgág vĩ ki jyjy pẽ",
-    "Wp/kgp/Kanhgág ãg kanhrãn fã pẽ",
-    "Wp/kgp/Kanhgág ũ tỹ ne né",
-    "Wp/kgp/Kanhkã tá ti nĩ",
-    "Wp/kgp/Kanẽ sĩ tavĩ",
-    "Wp/kgp/Kanẽsã to vẽme",
-    "Wp/kgp/Kapẽn tu vãme",
-    "Wp/kgp/Karugmág",
-    "Wp/kgp/Kasor to vême",
-    "Wp/kgp/Ketyjug tu vãme",
-    "Wp/kgp/Ko vãnh ũ ti garĩnh nér",
-    "Wp/kgp/Kri nĩj fã pénĩn vẽnhgrén",
-    "Wp/kgp/Kréj",
-    "Wp/kgp/Krĩg sĩnvĩ",
-    "Wp/kgp/Kukrej",
-    "Wp/kgp/Kumi",
-    "Wp/kgp/Kãj",
-    "Wp/kgp/Kãme kar Pénĩ",
-    "Wp/kgp/Kó'y Vãme",
-    "Wp/kgp/Kóhon",
-    "Wp/kgp/Main Page",
-    "Wp/kgp/Momro",
-    "Wp/kgp/Mĩg Vãme",
-    "Wp/kgp/NO",
-    "Wp/kgp/Ninsu",
-    "Wp/kgp/No kar vyj to vãme",
-    "Wp/kgp/Nãn ga",
-    "Wp/kgp/Nẽnkanh mré Nẽnẽ tu vãme",
-    "Wp/kgp/Nẽnẽ to Vẽme",
-    "Wp/kgp/Pego pego ( inh kâgmira, inh kâgmira)",
-    "Wp/kgp/Pirã",
-    "Wp/kgp/Pisé",
-    "Wp/kgp/Pãi ag to vãme",
-    "Wp/kgp/Pãri",
-    "Wp/kgp/Pãtá sỹm ke",
-    "Wp/kgp/Pého féj",
-    "Wp/kgp/Pénfág",
-    "Wp/kgp/Pénĩ",
-    "Wp/kgp/Pépo pẽn kókré",
-    "Wp/kgp/Pó tãpér tỹ vẽnh kanhir",
-    "Wp/kgp/Pẽnturó",
-    "Wp/kgp/Pẽnva",
-    "Wp/kgp/Pỹn",
-    "Wp/kgp/Rarỹnh Pãn",
-    "Wp/kgp/Rarỹnh kãme",
-    "Wp/kgp/Rijero ga",
-    "Wp/kgp/Rã",
-    "Wp/kgp/Seraj",
-    "Wp/kgp/Sukrĩg",
-    "Wp/kgp/Sukrĩg jógo",
-    "Wp/kgp/Sãpe to vẽme",
-    "Wp/kgp/To Vãme sĩ",
-    "Wp/kgp/To vãme",
-    "Wp/kgp/Tupẽ tỹ ẽg mré nĩn kỹ",
-    "Wp/kgp/Tusĩnh Tỹ mỹg pẽfyn",
-    "Wp/kgp/Van to vẽme",
-    "Wp/kgp/Voga",
-    "Wp/kgp/VÊNHPRŨG",
-    "Wp/kgp/Vãfy téj(tuja)balaio de tampa",
-    "Wp/kgp/Vãfy vãme",
-    "Wp/kgp/Vãnh Kãmĩ Grugru",
-    "Wp/kgp/Vãrensĩn",
-    "Wp/kgp/Vẽnh prũg to vãme",
-    "Wp/kgp/Vẽnhkagta",
-    "Wp/kgp/Vẽser to vẽme",
-    "Wp/kgp/Wp/kgp/",
-    "Wp/kgp/kumĩ",
-    "Wp/kgp/mỹnjóka",
-    "Wp/kgp/Ãma kãmĩ kanhgág jykre pẽ tu ke .",
-    "Wp/kgp/Ó",
-    "Wp/kgp/ẼMĨ",
-    "Wp/kgp/Ẽg Kygtãg",
-    "Wp/kgp/Ẽg rá",
-    "Wp/kgp/Ẽg vĩ ki alfabeto",
-    "Wp/kgp/Ẽkór tỹ ẽmῖ",
-    "Wp/kgp/Ẽmro",
-    "Wp/kgp/Ẽmã Ketyjug Tẽgtũ tu vãme sĩ",
-    "Wp/kgp/Ẽmã tỹ Karugmág vẽnh han kãme",
-]
+USER_AGENT = "Arcstur Scripts (https://meta.wikimedia.org/wiki/User:Arcstur)"
+ARTICLES_URL = "https://incubator.wikimedia.org/wiki/Special:PrefixIndex?prefix=Wp%2Fkgp%2F&namespace=0&hideredirects=1"
 
 
 def main():
+    titles = get_titles()
     title_to_revision = {}
     for title in titles:
         print(f"{title}...")
@@ -160,9 +38,26 @@ def main():
         export_articles(title_to_revision, f)
 
 
+def get_titles():
+    print(f"Making request to {ARTICLES_URL}...")
+    res = requests.get(
+        ARTICLES_URL, headers={"User-Agent": USER_AGENT, "Accept": "text/html"}
+    )
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "html.parser")
+    titles = []
+    anchors = soup.find("ul", class_="mw-prefixindex-list").find_all("a")
+    for a in anchors:
+        titles.append(a["title"])
+    return titles
+
+
 def get_first_revision(title):
     escaped_title = urllib.parse.quote(title, safe="")
-    res = requests.get(API + "page/" + escaped_title + "/history", headers=HEADERS)
+    res = requests.get(
+        API + "page/" + escaped_title + "/history",
+        headers={"User-Agent": USER_AGENT, "Accept": "application/json"},
+    )
     res.raise_for_status()
     data = res.json()
     first = data["revisions"][-1]
